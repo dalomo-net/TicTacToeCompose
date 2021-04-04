44f76005fa2ef856c8a8ab66974cf280e0acd87d
PIERCE <etreraviauseptiemeciel@gmail.com>
2021-04-04 14:24:07 +0900

    tight coupling

--- app/src/main/java/net/dalomo/tictactoecompose/MainActivity.kt
+++ app/src/main/java/net/dalomo/tictactoecompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -21,7 +22,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,8 +53,7 @@ class MainActivity : ComponentActivity() {
 
 @ExperimentalFoundationApi
 @Composable
-fun GameScreen(tttViewModel: TicTacToeViewModel = viewModel()) {
-    val arrOX: Array<Int> by tttViewModel.arrOX.observeAsState(arrayOf())
+fun GameScreen() {
 
     Column(
         modifier = Modifier
@@ -69,17 +68,17 @@ fun GameScreen(tttViewModel: TicTacToeViewModel = viewModel()) {
         LazyVerticalGrid(
             cells = GridCells.Fixed(3),
             modifier = Modifier
-                .size(240.dp)
+                .size((88 * 3).dp)
         ) {
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
-            item { Cell() }
+            item { Cell(0) }
+            item { Cell(1) }
+            item { Cell(2) }
+            item { Cell(3) }
+            item { Cell(4) }
+            item { Cell(5) }
+            item { Cell(6) }
+            item { Cell(7) }
+            item { Cell(8) }
         }
         ResetButton()
     }
@@ -95,41 +94,95 @@ fun GameScreen(tttViewModel: TicTacToeViewModel = viewModel()) {
 }
 
 @Composable
-fun Turn() {
+fun Turn(tttViewModel: TicTacToeViewModel = viewModel()) {
+
+    val turn = tttViewModel.turn.observeAsState(initial = 1)
+    val t = when (turn.value) {
+        1 -> {
+            "O Turn !"
+        }
+        -1 -> {
+            "X Turn !"
+        }
+        else -> "Finish !"
+    }
+
     Text(
-        text = "○" + " Turn !",
+        text = t,
         modifier = Modifier.fillMaxWidth(),
         style = TextStyle(fontSize = 40.sp, textAlign = TextAlign.Center)
     )
 }
 
 @Composable
-fun Cell() {
+fun Cell(index: Int, tttViewModel: TicTacToeViewModel = viewModel()) {
+    val OX0 = tttViewModel.OX0.observeAsState(0)
+    val OX1 = tttViewModel.OX1.observeAsState(0)
+    val OX2 = tttViewModel.OX2.observeAsState(0)
+    val OX3 = tttViewModel.OX3.observeAsState(0)
+    val OX4 = tttViewModel.OX4.observeAsState(0)
+    val OX5 = tttViewModel.OX5.observeAsState(0)
+    val OX6 = tttViewModel.OX6.observeAsState(0)
+    val OX7 = tttViewModel.OX7.observeAsState(0)
+    val OX8 = tttViewModel.OX8.observeAsState(0)
+
+    var str = ""
+
+    when (index) {
+        0 -> {
+            str = if (OX0.value == 1) "O" else if (OX0.value == -1) "X" else ""
+        }
+        1 -> {
+            str = if (OX1.value == 1) "O" else if (OX1.value == -1) "X" else ""
+        }
+        2 -> {
+            str = if (OX2.value == 1) "O" else if (OX2.value == -1) "X" else ""
+        }
+        3 -> {
+            str = if (OX3.value == 1) "O" else if (OX3.value == -1) "X" else ""
+        }
+        4 -> {
+            str = if (OX4.value == 1) "O" else if (OX4.value == -1) "X" else ""
+        }
+        5 -> {
+            str = if (OX5.value == 1) "O" else if (OX5.value == -1) "X" else ""
+        }
+        6 -> {
+            str = if (OX6.value == 1) "O" else if (OX6.value == -1) "X" else ""
+        }
+        7 -> {
+            str = if (OX7.value == 1) "O" else if (OX7.value == -1) "X" else ""
+        }
+        8 -> {
+            str = if (OX8.value == 1) "O" else if (OX8.value == -1) "X" else ""
+        }
+    }
+
     Box(
         modifier = Modifier
-            .size(80.dp)
+            .size(88.dp)
             .border(border = BorderStroke(width = 1.dp, color = Color.Black))
+            .clickable { tttViewModel.onClick(index) }
     ) {
         Text(
-            text = "O",
+            text = str,
             modifier = Modifier.fillMaxSize(),
             style = TextStyle(fontSize = 56.sp, textAlign = TextAlign.Center)
         )
     }
 }
 
-@Preview
 @Composable
-fun ResetButton() {
+fun ResetButton(tttViewModel: TicTacToeViewModel = viewModel()) {
     Button(
-        onClick = { /*TODO*/ },
+        onClick = { tttViewModel.resetBoard() },
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 48.dp)
+            .padding(top = 80.dp)
             .height(56.dp)
-            .clip(shape= Shapes.large )
+            .clip(shape = Shapes.large)
     ) {
-        Text(text = "Reset",style = TextStyle(fontSize = 24.sp))
+        Text(text = "Reset", style = TextStyle(fontSize = 24.sp))
     }
 }
 
--- app/src/main/java/net/dalomo/tictactoecompose/TicTacToeViewModel.kt
+++ app/src/main/java/net/dalomo/tictactoecompose/TicTacToeViewModel.kt
@@ -6,10 +6,85 @@ import androidx.lifecycle.ViewModel
 
 class TicTacToeViewModel : ViewModel() {
 
-    private val _arrOX = MutableLiveData(arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0))
-    val arrOX: LiveData<Array<Int>> = _arrOX
+    private val _OX0 = MutableLiveData(0)
+    var OX0: LiveData<Int> = _OX0
+    private val _OX1 = MutableLiveData(0)
+    var OX1: LiveData<Int> = _OX1
+    private val _OX2 = MutableLiveData(0)
+    var OX2: LiveData<Int> = _OX2
+    private val _OX3 = MutableLiveData(0)
+    var OX3: LiveData<Int> = _OX3
+    private val _OX4 = MutableLiveData(0)
+    var OX4: LiveData<Int> = _OX4
+    private val _OX5 = MutableLiveData(0)
+    var OX5: LiveData<Int> = _OX5
+    private val _OX6 = MutableLiveData(0)
+    var OX6: LiveData<Int> = _OX6
+    private val _OX7 = MutableLiveData(0)
+    var OX7: LiveData<Int> = _OX7
+    private val _OX8 = MutableLiveData(0)
+    var OX8: LiveData<Int> = _OX8
 
-    fun onClicked(index: Int) {
-        _arrOX.value?.set(index, 0)
+    private val _turn = MutableLiveData(1)
+    var turn: LiveData<Int> = _turn
+
+    fun onClick(index: Int) {
+        when (index) {
+            0 -> {
+                if (_OX0.value == 0)
+                    _OX0.value = turn.value
+            }
+            1 -> {
+                if (_OX1.value == 0)
+                    _OX1.value = turn.value
+            }
+            2 -> {
+                if (_OX2.value == 0)
+                    _OX2.value = turn.value
+            }
+            3 -> {
+                if (_OX3.value == 0)
+                    _OX3.value = turn.value
+            }
+            4 -> {
+                if (_OX4.value == 0)
+                    _OX4.value = turn.value
+            }
+            5 -> {
+                if (_OX5.value == 0)
+                    _OX5.value = turn.value
+            }
+            6 -> {
+                if (_OX6.value == 0)
+                    _OX6.value = turn.value
+            }
+            7 -> {
+                if (_OX7.value == 0)
+                    _OX7.value = turn.value
+            }
+            8 -> {
+                if (_OX8.value == 0)
+                    _OX8.value = turn.value
+            }
+        }
+        _turn.value = _turn.value!! * (-1)
+
+        if (_OX0.value != 0 && _OX1.value!! != 0 && _OX2.value!! != 0 && _OX3.value!! != 0 && _OX4.value!! != 0 && _OX5.value!! != 0 && _OX6.value!! != 0 && _OX7.value!! != 0 && _OX8.value!! != 0) {
+            _turn.value = 0
+        }
+    }
+
+    fun resetBoard() {
+        _OX0.value = 0
+        _OX1.value = 0
+        _OX2.value = 0
+        _OX3.value = 0
+        _OX4.value = 0
+        _OX5.value = 0
+        _OX6.value = 0
+        _OX7.value = 0
+        _OX8.value = 0
+
+        _turn.value = 1
     }
 }
